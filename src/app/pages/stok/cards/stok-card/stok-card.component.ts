15337e3ef36d1ae70de994a3e66b7299240b8255
Recep-Yigiter <rcpyr06@gmail.com>
2023-09-21 11:40:22 +0300

    recep

--- src/app/pages/stok/cards/stok-card/stok-card.component.ts
+++ src/app/pages/stok/cards/stok-card/stok-card.component.ts
@@ -35,11 +35,27 @@ export class StokCardComponent implements OnInit {
   totalRecords: number;
   totalHesap: any;
   selectedNode = undefined;
-  selectedReceteItem: any;
+
   selectedCategory: any = "1";
   toggleVisible: any;
   treeViewChildList: any;
-  treeItemSelect: any;
+
+  stokDetayList: any = [
+    // { label: "Stok Kodu", value: this.selectedNode?.kod },
+    // { label: "Stok Kısa Adı ", value: null },
+    // { label: "Stok Adı", value: this.selectedNode?.ad },
+    // { label: "Stok Türü", value: null },
+    // { label: "Sipariş İhtiyaç Seviyesi", value: null },
+    // { label: "Birim1 Adı ", value: this.selectedNode?.birimAdi },
+    // { label: "Birim2 Adı ", value: null },
+    // { label: "Birim3 Adı ", value: null },
+    // { label: "Katsayı ", value: null },
+    // { label: "Alış KDV Oranı ", value: null },
+    // { label: "Satış KDV Oranı ", value: null },
+    // { label: "Alış Vade Süresi", value: null },
+    // { label: "Satış Vade Süresi", value: null },
+    // { label: "Satır İskonto", value: "Uygulanır" },
+  ]
   @ViewChild('first') matRadioButton: MatRadioButton;
   constructor(
     private StokService: StokService,
@@ -48,10 +64,9 @@ export class StokCardComponent implements OnInit {
   ) {
     this.toggleVisible = this.StokTreeViewService.toggleVisible
   }
-  ngOnInit(): void {
 
+  ngOnInit(): void {
     this.TreeViewList()
-    this.GetList()
   }
 
   async TreeViewList() {
@@ -69,94 +84,66 @@ export class StokCardComponent implements OnInit {
     var tree = this.StokTreeViewService.CreateTreeView(this.treeViewDataSource.data.items);
     this.menu = tree.map(x => this.StokTreeViewService.toNode(x));
 
+   this.selectedNode=this.menu[0];
+
+   this.stokDetayList = [
+    { label: "Stok Kodu", value: this.selectedNode?.kod },
+    { label: "Stok Kısa Adı ", value: null },
+    { label: "Stok Adı", value: this.selectedNode?.ad },
+    { label: "Stok Türü", value: null },
+    { label: "Sipariş İhtiyaç Seviyesi", value: null },
+    { label: "Birim1 Adı ", value: this.selectedNode?.birimAdi },
+    { label: "Birim2 Adı ", value: null },
+    { label: "Birim3 Adı ", value: null },
+    { label: "Katsayı ", value: null },
+    { label: "Alış KDV Oranı ", value: null },
+    { label: "Satış KDV Oranı ", value: null },
+    { label: "Alış Vade Süresi", value: null },
+    { label: "Satış Vade Süresi", value: null },
+    { label: "Satır İskonto", value: "Uygulanır" },
+  ]
 
 
   }
-  async GetList() {
-    const getList = await this.StokService.GetList()
-    this.dataSource = getList.data.items;
-    this.totalRecords = getList.count;
-  }
-
   addDialog() {
     this.StokCardDialogService.addDialog(() => {
       this.TreeViewList();
-      this.GetList();
     })
   }
-
-  updateDialog() {
-    this.StokCardDialogService.updateDialog(() => {
-      this.TreeViewList();
-      this.GetList();
-    })
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  TreeItem(item: any) {
-
-    this.treeViewChildList = this.StokTreeViewService.treeViewChildList(item)
-  }
-
-  TreeItemSelect(item) {
-
-    this.treeItemSelect = item
-
-  }
-
-
-  selectNode(node: any) {
-    this.selectedNode = node;
-  }
-
-
-
-
-
-
-
-
-
-
-  cardAdd(event) {
-    this.StokCardDialogService.addDialog(() => {
-
-      this.TreeViewList()
-    })
-
-  }
-  childCardAdd(event) {
-    if (this.treeItemSelect != undefined) {
-      this.StokCardDialogService.addChildDialog(this.treeItemSelect, () => {
+  addChildDialog(){
+    if (this.selectedNode != undefined) {
+      this.StokCardDialogService.addChildDialog(this.selectedNode, () => {
 
         this.TreeViewList()
       })
     }
-
+  }
+  updateDialog() {
+    this.StokCardDialogService.updateDialog(() => {
+      this.TreeViewList();
+    })
+  }
+  selectNode(node: any) {
+    this.stokDetayList = []
+    this.selectedNode = node;
+    this.stokDetayList = [
+      { label: "Stok Kodu", value: this.selectedNode?.kod },
+      { label: "Stok Kısa Adı ", value: null },
+      { label: "Stok Adı", value: this.selectedNode?.ad },
+      { label: "Stok Türü", value: null },
+      { label: "Sipariş İhtiyaç Seviyesi", value: null },
+      { label: "Birim1 Adı ", value: this.selectedNode?.birimAdi },
+      { label: "Birim2 Adı ", value: null },
+      { label: "Birim3 Adı ", value: null },
+      { label: "Katsayı ", value: null },
+      { label: "Alış KDV Oranı ", value: null },
+      { label: "Satış KDV Oranı ", value: null },
+      { label: "Alış Vade Süresi", value: null },
+      { label: "Satış Vade Süresi", value: null },
+      { label: "Satır İskonto", value: "Uygulanır" },
+    ]
 
   }
-  StokItem: any;
-  onRowSelect(event: any) {
-    this.StokItem = event.data;
-
-  }
-  onRowUnSelect() {
-    this.StokItem = null
-  }
-
-
 
 
 
